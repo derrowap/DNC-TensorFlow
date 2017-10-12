02f842d57151eb17645ec4e082c9c52c9e4820cd
Austin Derrow-Pinion <derrowap@gmail.com>
2017-10-12 02:01:18 -0400

    Supports batch_size in content addressing in external memory. Conforms to python linters Flake8, pydocstyle, pep8-naming, and flake8-import-order (Google style).

--- src/dnc/external_memory.py
+++ src/dnc/external_memory.py
@@ -8,8 +8,8 @@ Author: Austin Derrow-Pinion
 """
 
 import collections
-import sonnet as snt
 import tensorflow as tf
+import sonnet as snt
 
 # Ensure values are greater than epsilon to avoid numerical instability.
 _EPSILON = 1e-6
@@ -17,24 +17,25 @@ _EPSILON = 1e-6
 ExternalMemoryState = collections.namedtuple('ExternalMemoryState', (
     'memory'))
 
+
 class ExternalMemory(snt.RNNCore):
     """External Memory for the DNC to read and write to.
-    
+
     In a content based addressing read mode, a DNC read head compares
     a read vector to the contents stored in this External Memory. This
     similarity is computed using cosine similarity in the DNC paper.
-    
+
     A write head writes to external memory in a DNC through first an
     erase operation and then an adding operation. The erase operation
     adds an erase vector across each memory slot and is then followed
     by adding the write vector to memory.
     """
-    
+
     def __init__(self,
                  memory_size=16,
                  word_size=16,
                  name='external_memory'):
-        """Initializes External Memory used in a DNC.
+        """Initialize External Memory used in a DNC.
 
         Args:
             memory_size: The number of memory slots (N in the DNC paper).
@@ -44,77 +45,97 @@ class ExternalMemory(snt.RNNCore):
             name: The name of the module (default 'external_memory').
         """
         super(ExternalMemory, self).__init__(name=name)
-        
+
         self._memory_size = memory_size
         self._word_size = word_size
-        
+
         self._state_size = ExternalMemoryState(
             memory=tf.TensorShape([self._memory_size, self._word_size]))
-    
+
     def _build(self,
                read_keys,
                read_strengths,
                prev_state):
-        """Computes one timestep of computation for the External Memory.
-        
-        Content based addressing starts with calculating the cosine
-        similarity between the input read keys and the current state of
-        memory. The content vector, `c_t`, at time `t` is outputed by
-        taking the softmax of the product between the cosine similarity and
-        the input read strengths.
-        
+        """Compute one timestep of computation for the External Memory.
+
         Args:
-            read_keys: A Tensor of shape `[word_size]` containing the read
-                keys from the Read Head originally emitted by the DNC
-                controller. These are compared to each slot in the external
-                memory for content based addressing.
-            read_strengths: A Tensor of shape `[memory_size]` containing the
-                strength at which the `c_t` vector should favor each slot in
-                external memory. If `read_strengths[i]` is high, then
-                `c_t[i]` will be greater than if `read_strengths[i]` was a
-                small value. The values are bound to the interval [1, inf).
+            read_keys: A Tensor of shape `[batch_size, num_reads, word_size]`
+                containing the read keys from the Read Head originally emitted
+                by the DNC controller. These are compared to each slot in the
+                external memory for content based addressing.
+            read_strengths: A Tensor of shape `[batch_size, num_reads]`
+                containing the key strength at which the `c_t` vector should
+                be heightened in value. The values are bound to the interval
+                [1, inf). Represented in the DNC paper as `beta_t^{r,i}` for
+                time `t` with the ith read head.
             prev_state: An instance of `ExternalMemoryState` containing the
                 previous state of this External Memory.
         Returns:
-            A tuple `(output, next_state)`. Where `output` is a Tensor of
-            the content based addressing weightings. The `next_state` is
-            an instance of `ExternalMemoryState` representing the next state
-            of this External Memory after computation finishes.
+            A tuple `(output, next_state)`. Where `output` is a Tensor of the
+            content based addressing weightings. The `next_state` is an
+            instance of `ExternalMemoryState` representing the next state of
+            this External Memory after computation finishes.
         """
-        repeated_read_keys = tf.reshape(tf.tile(read_keys,
-                                                [self._memory_size]),
-                                        [self._memory_size, self._word_size])
-        content_similarity = self.cosine_similarity(repeated_read_keys,
-                                                    prev_state.memory)
-        c_t = tf.nn.softmax(tf.multiply(content_similarity, read_strengths))
-        
+        c_t = self.content_weights(
+            read_keys, read_strengths, prev_state.memory)
         return (c_t, prev_state)
-    
-    def cosine_similarity(self, u, v):
-        """Computes the cosine similarity between two vectors, `u` and `v`.
-        
+
+    def content_weights(self, read_keys, read_strengths, memory):
+        """Calculate content based addressing weights, c_t.
+
+        Content based addressing starts with calculating the cosine similarity
+        between the input read keys and the current state of memory. The
+        content vector, `c_t`, at time `t` is outputed by taking the softmax of
+        the product between the cosine similarity and the input read strengths.
+
         Cosine similarity here is defined as in the DNC paper:
             D(u, v) = (u * v) / (|u| * |v|)
         The resulting similarity ranges from -1 meaning exactly opposite, to
         1 meaning exactly the same.
-        
+
         Args:
-            u: A 2-D Tensor of shape `[batch_size, word_size]`.
-            v: A 2-D Tensor of shape `[batch_size, word_size]`.
+            read_keys: A Tensor of shape `[batch_size, num_reads, word_size]`
+                containing the read keys from the Read Head originally emitted
+                by the DNC controller. These are compared to each slot in the
+                external memory for content based addressing.
+            read_strengths: A Tensor of shape `[batch_size, num_reads]`
+                containing the key strength at which the `c_t` vector should
+                be heightened in value. The values are bound to the interval
+                [1, inf). Represented in the DNC paper as `beta_t^{r,i}` for
+                time `t` with the ith read head.
+            memory: A Tensor of shape `[batch_size, memory_size, word_size]`
+                containing the data of the external memory.
         Returns:
-            A Tensor of shape `[batch_size]` containing the cosine similarity
-            for the two input vectors, `u` and `v`.
+            A Tensor of shape `[batch_size, num_reads, memory_size]` with the
+            content weights for each of the num_reads many read heads.
         """
-        dot = tf.reduce_sum(tf.multiply(u, v), 1)
-        norm = tf.norm(u, ord='euclidean', axis=1) * tf.norm(v, ord='euclidean', axis=1)
-        return dot / (norm + tf.constant(_EPSILON))
-    
+        # Sneaky way of doing batched dot product between read_keys and memory.
+        # Each read key, k_t^{r,i}, is element-wise multiplied against each row
+        # of memory and summed together (definition of dot product). This
+        # outputs a Tensor of size `[batch_size, num_reads, memory_size]`.
+        dot = tf.matmul(read_keys, memory, transpose_b=True)
+
+        # Euclidean norms
+        read_keys_norm = tf.norm(
+            read_keys, ord='euclidean', axis=2, keep_dims=True)
+        memory_norm = tf.norm(memory, ord='euclidean', axis=2, keep_dims=True)
+        norm = tf.matmul(read_keys_norm, memory_norm, transpose_b=True)
+
+        # similarity of each read key vector from each read head in every batch
+        content_similarity = dot / (norm + tf.constant(_EPSILON))
+
+        # Content weighting is a softmax of the content similarity.
+        # The product of content_similarity and read_strenghts uses
+        # broadcasting to handle unequal Tensor shapes.
+        return tf.nn.softmax(tf.multiply(content_similarity,
+                                         tf.expand_dims(read_strengths, 2)))
+
     @property
     def state_size(self):
-        """Returns a description of the state size."""
+        """Return a description of the state size."""
         return self._state_size
-    
+
     @property
     def output_size(self):
-        """Returns the output shape."""
+        """Return the output shape."""
         return tf.TensorShape([self._memory_size])
--- src/testing/external_memory_test.py
+++ src/testing/external_memory_test.py
@@ -3,138 +3,175 @@
 import tensorflow as tf
 import unittest
 
-from .. dnc.external_memory import *
+from .. dnc import external_memory
+
 
 def suite():
+    """Create testing suite for all tests in this module."""
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ExternalMemoryTest))
     return suite
 
+
 class ExternalMemoryTest(unittest.TestCase):
-    
+    """Tests for the ExternalMemory class."""
+
     def test_construction(self):
+        """Test the construction of an ExternalMemory."""
+        memory = external_memory.ExternalMemory()
+        self.assertIsInstance(memory, external_memory.ExternalMemory)
+
+    def test_content_weights(self):
+        """Test content_weights function."""
         graph = tf.Graph()
         with graph.as_default():
             with tf.Session(graph=graph) as sess:
-                memory = ExternalMemory()
-                self.assertIsInstance(memory, ExternalMemory)
-                
-    def test_cosine_similarity(self):
-        graph = tf.Graph()
-        with graph.as_default():
-            with tf.Session(graph=graph) as sess:
-                memory = ExternalMemory()
-                diff = 1e-3 # take into account floating precision
+                diff = 1e-4  # take into account floating precision
                 tests = [
-                    { # simple example
-                        'a': tf.constant([[1, 1, 0]], dtype=tf.float32),
-                        'b': tf.constant([[1, 0, 1]], dtype=tf.float32),
-                        'expected': [0.5],
-                    }, { # divide by zero
-                        'a': tf.constant([[0, 0, 0]], dtype=tf.float32),
-                        'b': tf.constant([[1, 1, 1]], dtype=tf.float32),
-                        'expected': [0],
-                    }, { # equal vectors
-                        'a': tf.constant([[100, 100, 100, 100]], dtype=tf.float32),
-                        'b': tf.constant([[100, 100, 100, 100]], dtype=tf.float32),
-                        'expected': [1],
-                    }, { # batch size > 1
-                        'a': tf.constant([
-                            [1, 2, 3, 4, 5],
-                            [120, 2, 40.5, 1, 1],
-                        ], dtype=tf.float32),
-                        'b': tf.constant([
-                            [6, 7, 8, 9, 10],
-                            [3, 60, 111, 1, 1],
-                        ], dtype=tf.float32),
-                        'expected': [0.9650, 0.3113],
-                    }, { # cosine similarity produces a 1 for any 1-lengthed vector
-                         # because the product and norm products are equal.
-                        'a': tf.constant([[1], [2], [3], [4], [5], [6], [7]],
-                                         dtype=tf.float32),
-                        'b': tf.constant([[4], [4], [4], [4], [4], [4], [4]],
-                                         dtype=tf.float32),
-                        'expected': [1, 1, 1, 1, 1, 1, 1],
+                    {  # equal content addressed probabilities
+                        'memory': [[[1, 0, 1], [1, 0, 1], [1, 0, 1]]],
+                        'read_keys': [[[1, 1, 1]]],
+                        'read_strengths': [[1]],
+                        'equal_indices': [[[0, 1, 2]]],
+                        'indices_sorted': [],
+                    }, {  # similar slots of memory have greater values in c_t
+                        'memory': [[[1, 1, 1], [1, 0, 1], [0, 0, 1]]],
+                        'read_keys': [[[1, 1, 1]]],
+                        'read_strengths': [[1]],
+                        'equal_indices': [],
+                        'indices_sorted': [[[0, 1, 2]]],
+                    }, {  # unequal dimensions of memory for NxM
+                        'memory': [[[-5, -4], [-3, -2], [-1, 0], [1, 2],
+                                    [3, 4]]],
+                        'read_keys': [[[-1, 0], [-1, 0]]],
+                        'read_strengths': [[1]],
+                        'equal_indices': [],
+                        'indices_sorted': [[[2, 3, 4], [2, 1, 0]]],
+                    }, {  # independent slots are equal in probability
+                        'memory': [[[1, 2], [50, 60], [1, 2], [-30, 70],
+                                    [1, 2], [120, 85]]],
+                        'read_keys': [[[1, 2]]],
+                        'read_strengths': [[1]],
+                        'equal_indices': [[[0, 2, 4]]],
+                        'indices_sorted': [],
+                    }, {  # tests that num_reads > 1 works as expected
+                        'memory': [[[1, 1, 1], [1, 1, 1],
+                                    [1, 1, 1], [1, 1, 1]]],
+                        'read_keys': [[[1, 1, 1], [1, 1, 1], [1, 1, 1]]],
+                        'read_strengths': [[1, 1, 1]],
+                        'equal_indices': [[[0, 1, 2], [0, 1, 2], [0, 1, 2]]],
+                        'indices_sorted': [],
+                    }, {  # test batch sizes work as expected
+                        'memory': [[[1, 0, 1], [1, 0, 1], [1, 0, 1]],
+                                   [[1, 1, 1], [1, 0, 1], [0, 0, 1]],
+                                   [[1, 1, 1], [1, 0, 1], [0, 0, 1]]],
+                        'read_keys': [[[1, 1, 1]], [[1, 1, 1]], [[0, 0, 1]]],
+                        'read_strengths': [[1], [1], [1]],
+                        'equal_indices': [[[0, 1, 2]], [], []],
+                        'indices_sorted': [[], [[0, 1, 2]], [[2, 1, 0]]],
                     },
                 ]
                 for test in tests:
-                    a = test['a']
-                    b = test['b']
-                    expected = test['expected']
-                    similarities = sess.run(memory.cosine_similarity(a, b))
-                    self.assertEqual(a.shape, b.shape)
-                    self.assertEqual(similarities.shape[0], sess.run(a).shape[0])
-                    for want, actual in zip(expected, similarities):
-                        self.assertTrue(abs(want - actual) <= diff,
-                                   msg="Test {}: expected {}, got {}".format(\
-                                    tests.index(test), want, actual))
-                        
-    def test_content_based_read(self):
-        graph = tf.Graph()
-        with graph.as_default():
-            with tf.Session(graph=graph) as sess:
-                diff = 1e-4 # take into account floating precision
-                tests = [
-                    { # equal content addressed probabilities
-                        'memory': [[1, 0, 1], [1, 0, 1], [1, 0, 1]],
-                        'read_keys': [1, 1, 1],
-                        'read_strengths': [1, 1, 1],
-                        'equal_indices': [[0, 1, 2]],
-                        'indices_sorted': [],
-                    }, { # more similar slots of memory have greater values in c_t
-                        'memory': [[1, 1, 1], [1, 0, 1], [0, 0, 1]],
-                        'read_keys': [1, 1, 1],
-                        'read_strengths': [1, 1, 1],
-                        'equal_indices': [],
-                        'indices_sorted': [[0, 1, 2]],
-                    }, { # unequal dimensions of memory for NxM
-                        'memory': [[-5, -4], [-3, -2], [-1, 0], [1, 2], [3, 4]],
-                        'read_keys': [-1, 0],
-                        'read_strengths': [1, 1, 1, 1, 1],
-                        'equal_indices': [],
-                        'indices_sorted': [[2, 3, 4], [2, 1, 0]],
-                    }, { # independent slots are equal in probability
-                        'memory': [[1, 2], [50, 60], [1, 2], [-30, 70], [1, 2], [120, 85]],
-                        'read_keys': [1, 2],
-                        'read_strengths': [1, 1, 1, 1, 1, 1],
-                        'equal_indices': [[0, 2, 4]],
-                        'indices_sorted': [],
-                    }, { # read strengths heighten output with greater strengths
-                        'memory': [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],
-                        'read_keys': [1, 1, 1],
-                        'read_strengths': [1, 2, 3, 4],
-                        'equal_indices': [],
-                        'indices_sorted': [[3, 2, 1, 0]],
-                    },
-                ]
-                for test in tests:
-                    memory = ExternalMemory(memory_size=len(test['memory']),
-                                            word_size=len(test['memory'][0]))
-                    prev_state = ExternalMemoryState(
-                        memory=tf.constant(test['memory'], dtype=tf.float32))
-                    read_keys = tf.constant(test['read_keys'], dtype=tf.float32)
-                    read_strengths = tf.constant(test['read_strengths'], dtype=tf.float32)
-                    c_t_op, next_state = memory(read_keys, read_strengths, prev_state)
+                    mem = external_memory.ExternalMemory(
+                        memory_size=len(test['memory'][0]),
+                        word_size=len(test['memory'][0][0]))
+                    memory = tf.constant(test['memory'], dtype=tf.float32)
+                    read_keys = tf.constant(test['read_keys'],
+                                            dtype=tf.float32)
+                    read_strengths = tf.constant(test['read_strengths'],
+                                                 dtype=tf.float32)
+                    c_t_op = mem.content_weights(read_keys, read_strengths,
+                                                 memory)
                     c_t = sess.run(c_t_op)
-                    for equal_indices in test['equal_indices']:
-                        for i in range(len(equal_indices) - 1):
-                            index1 = equal_indices[i]
-                            index2 = equal_indices[i+1]
-                            self.assertTrue(abs(c_t[index1] - c_t[index2]) <= diff,
-                                            msg="Test {}: expected index {} and {} to be equal "\
-                                                "but were {} and {}".format(tests.index(test),
-                                                                            index1,
-                                                                            index2,
-                                                                            c_t[index1],
-                                                                            c_t[index2]))
-                    for sorted in test['indices_sorted']:
-                        for i in range(len(sorted) - 1):
-                            index1 = sorted[i]
-                            index2 = sorted[i+1]
-                            self.assertTrue(c_t[index1] > c_t[index2],
-                                            msg="Test {}: c_t[{}]={} not > c_t[{}]={}".format(
-                                                tests.index(test), index1, c_t[index1],
-                                                index2, c_t[index2]))
+                    batch_num = 0
+                    for equal_indices_batch in test['equal_indices']:
+                        read_num = 0
+                        for equal_indices in equal_indices_batch:
+                            for i in range(len(equal_indices) - 1):
+                                index1 = equal_indices[i]
+                                index2 = equal_indices[i + 1]
+                                self.assertTrue(
+                                    abs(c_t[batch_num][read_num][index1] -
+                                        c_t[batch_num][read_num][index2]) <=
+                                    diff,
+                                    msg="Test {}: batch {} -> expected index"
+                                        " {} and {} to be equal but were {} "
+                                        "and {}".format(
+                                            tests.index(test), batch_num,
+                                            index1, index2,
+                                            c_t[batch_num][read_num][index1],
+                                            c_t[batch_num][read_num][index2]))
+                            read_num += 1
+                        batch_num += 1
+                    batch_num = 0
+                    for sorted_batch in test['indices_sorted']:
+                        read_num = 0
+                        for sorted in sorted_batch:
+                            for i in range(len(sorted) - 1):
+                                index1 = sorted[i]
+                                index2 = sorted[i + 1]
+                                self.assertTrue(
+                                    c_t[batch_num][read_num][index1] >
+                                    c_t[batch_num][read_num][index2],
+                                    msg="Test {}: batch {} -> c_t[{}]={} not >"
+                                        " c_t[{}]={}".format(
+                                            tests.index(test), batch_num,
+                                            index1,
+                                            c_t[batch_num][read_num][index1],
+                                            index2,
+                                            c_t[batch_num][read_num][index2]))
+                            read_num += 1
+                        batch_num += 1
+
+    def test_content_weights_read_strenghts(self):
+        """Test content_weights function for effective read_strenghts."""
+        graph = tf.Graph()
+        with graph.as_default():
+            with tf.Session(graph=graph) as sess:
+                diff = 1e-4  # take into account floating precision
+                tests = [
+                    {  # tests that num_reads > 1 works as expected
+                        'memory': [[[1, 1, 1], [1, 1, 1],
+                                    [1, 1, 1], [1, 1, 1]]],
+                        'read_keys': [[[1, 1, 1], [1, 1, 1], [1, 1, 1]]],
+                        'read_strengths': [[1, 2, 3]],
+                        'indices_sorted': [[0, 1, 2]],
+                    }, {
+                        'memory': [[[1, 1], [1, 1]], [[1, 1], [1, 1]]],
+                        'read_keys': [[[1, 1], [1, 1]], [[1, 1], [1, 1]]],
+                        'read_strengths': [[1, 2], [2, 1]],
+                        'indices_sorted': [[0, 1], [1, 0]],
+                    },
+                ]
+                for test in tests:
+                    mem = external_memory.ExternalMemory(
+                        memory_size=len(test['memory'][0]),
+                        word_size=len(test['memory'][0][0]))
+                    memory = tf.constant(test['memory'], dtype=tf.float32)
+                    read_keys = tf.constant(test['read_keys'],
+                                            dtype=tf.float32)
+                    read_strengths = tf.constant(test['read_strengths'],
+                                                 dtype=tf.float32)
+                    c_t_op = mem.content_weights(read_keys, read_strengths,
+                                                 memory)
+                    c_t = sess.run(c_t_op)
+                    batch_num = 0
+                    for sorted_reads in test['indices_sorted']:
+                        for i in range(len(sorted_reads) - 1):
+                            index1 = sorted_reads[i]
+                            index2 = sorted_reads[i + 1]
+                            for j in range(len(c_t[batch_num][index1])):
+                                self.assertTrue(
+                                    abs(c_t[batch_num][index1][j] -
+                                        c_t[batch_num][index2][j]) <= diff,
+                                    msg="Test {}: expected c_t[{}][{}][{}] = "
+                                        "{} == c_t[{}][{}][{}] = {}".format(
+                                            tests.index(test),
+                                            batch_num, index1, j,
+                                            c_t[batch_num][index1][j],
+                                            batch_num, index2, j,
+                                            c_t[batch_num][index2][j]))
+                        batch_num += 1
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
