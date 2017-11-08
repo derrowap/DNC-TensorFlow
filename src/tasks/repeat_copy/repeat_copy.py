bf56526d3224730da7929c021bcb939f12c3cd0c
Austin Derrow-Pinion <derrowap@gmail.com>
2017-11-08 12:45:34 -0500

    Reformatted RepeatCopy to my train.py standards.

--- src/tasks/repeat_copy/repeat_copy.py
+++ src/tasks/repeat_copy/repeat_copy.py
@@ -183,7 +183,7 @@ class RepeatCopy(snt.AbstractModule):
 
         Args:
             name: A name for the generator instance (for name scope purposes).
-                num_bits: The dimensionality of each random binary vector.
+            num_bits: The dimensionality of each random binary vector.
             batch_size: Minibatch size per realization.
             min_length: Lower limit on number of random binary vectors in the
                 observation pattern.
@@ -215,197 +215,200 @@ class RepeatCopy(snt.AbstractModule):
         self._log_prob_in_bits = log_prob_in_bits
         self._time_average_cost = time_average_cost
 
-        def _normalise(self, val):
-            return val / self._norm_max
+    def _normalise(self, val):
+        return val / self._norm_max
 
-        def _unnormalise(self, val):
-            return val * self._norm_max
+    def _unnormalise(self, val):
+        return val * self._norm_max
 
-        @property
-        def time_average_cost(self):
-            """The time average cost of this task."""
-            return self._time_average_cost
+    @property
+    def time_average_cost(self):
+        """The time average cost of this task."""
+        return self._time_average_cost
 
-        @property
-        def log_prob_in_bits(self):
-            """The log-probability in bits."""
-            return self._log_prob_in_bits
+    @property
+    def log_prob_in_bits(self):
+        """The log-probability in bits."""
+        return self._log_prob_in_bits
 
-        @property
-        def num_bits(self):
-            """The dimensionality of each random binary vector in a pattern."""
-            return self._num_bits
+    @property
+    def num_bits(self):
+        """The dimensionality of each random binary vector in a pattern."""
+        return self._num_bits
 
-        @property
-        def target_size(self):
-            """The dimensionality of the target tensor."""
-            return self._num_bits + 1
+    @property
+    def output_size(self):
+        """The dimensionality of the target tensor."""
+        return self._num_bits + 1
 
-        @property
-        def batch_size(self):
-            """The batch size of this task."""
-            return self._batch_size
+    @property
+    def batch_size(self):
+        """The batch size of this task."""
+        return self._batch_size
 
-        def _build(self):
-            """Implement build method which adds ops to graph."""
-            # short-hand for private fields.
-            min_length, max_length = self._min_length, self._max_length
-            min_reps, max_reps = self._min_repeats, self._max_repeats
-            num_bits = self.num_bits
-            batch_size = self.batch_size
+    def _build(self):
+        """Implement build method which adds ops to graph."""
+        # short-hand for private fields.
+        min_length, max_length = self._min_length, self._max_length
+        min_reps, max_reps = self._min_repeats, self._max_repeats
+        num_bits = self.num_bits
+        batch_size = self.batch_size
 
-            # We reserve one dimension for the num-repeats and one for the
-            # start-marker.
-            full_obs_size = num_bits + 2
-            # We reserve one target dimension for the end-marker.
-            full_targ_size = num_bits + 1
-            start_end_flag_idx = full_obs_size - 2
-            num_repeats_channel_idx = full_obs_size - 1
+        # We reserve one dimension for the num-repeats and one for the
+        # start-marker.
+        full_obs_size = num_bits + 2
+        # We reserve one target dimension for the end-marker.
+        full_targ_size = num_bits + 1
+        start_end_flag_idx = full_obs_size - 2
+        num_repeats_channel_idx = full_obs_size - 1
 
-            # Samples each batch index's sequence length and the number of
-            # repeats.
-            sub_seq_length_batch = tf.random_uniform(
-                [batch_size], minval=min_length, maxval=max_length + 1,
-                dtype=tf.int32)
-            num_repeats_batch = tf.random_uniform(
-                [batch_size], minval=min_reps, maxval=max_reps + 1,
-                dtype=tf.int32)
+        # Samples each batch index's sequence length and the number of
+        # repeats.
+        sub_seq_length_batch = tf.random_uniform(
+            [batch_size], minval=min_length, maxval=max_length + 1,
+            dtype=tf.int32)
+        num_repeats_batch = tf.random_uniform(
+            [batch_size], minval=min_reps, maxval=max_reps + 1,
+            dtype=tf.int32)
 
-            # Pads all the batches to have the same total sequence length.
-            total_length_batch = sub_seq_length_batch * \
-                (num_repeats_batch + 1) + 3
-            max_length_batch = tf.reduce_max(total_length_batch)
-            residual_length_batch = max_length_batch - total_length_batch
+        # Pads all the batches to have the same total sequence length.
+        total_length_batch = sub_seq_length_batch * \
+            (num_repeats_batch + 1) + 3
+        max_length_batch = tf.reduce_max(total_length_batch)
+        residual_length_batch = max_length_batch - total_length_batch
 
-            obs_batch_shape = [max_length_batch, batch_size, full_obs_size]
-            targ_batch_shape = [max_length_batch, batch_size, full_targ_size]
-            mask_batch_trans_shape = [batch_size, max_length_batch]
+        obs_batch_shape = [max_length_batch, batch_size, full_obs_size]
+        targ_batch_shape = [max_length_batch, batch_size, full_targ_size]
+        mask_batch_trans_shape = [batch_size, max_length_batch]
 
-            obs_tensors = []
-            targ_tensors = []
-            mask_tensors = []
+        obs_tensors = []
+        targ_tensors = []
+        mask_tensors = []
 
-            # Generates patterns for each batch element independently.
-            for batch_index in range(batch_size):
-                sub_seq_len = sub_seq_length_batch[batch_index]
-                num_reps = num_repeats_batch[batch_index]
+        # Generates patterns for each batch element independently.
+        for batch_index in range(batch_size):
+            sub_seq_len = sub_seq_length_batch[batch_index]
+            num_reps = num_repeats_batch[batch_index]
 
-                # The observation pattern is a sequence of random binary
-                # vectors.
-                obs_pattern_shape = [sub_seq_len, num_bits]
-                obs_pattern = tf.cast(
-                    tf.random_uniform(
-                        obs_pattern_shape, minval=0, maxval=2, dtype=tf.int32),
-                    tf.float32)
+            # The observation pattern is a sequence of random binary
+            # vectors.
+            obs_pattern_shape = [sub_seq_len, num_bits]
+            obs_pattern = tf.cast(
+                tf.random_uniform(
+                    obs_pattern_shape, minval=0, maxval=2, dtype=tf.int32),
+                tf.float32)
 
-                # The target pattern is the observation pattern repeated n
-                # times. Some reshaping is required to accomplish the tiling.
-                targ_pattern_shape = [sub_seq_len * num_reps, num_bits]
-                flat_obs_pattern = tf.reshape(obs_pattern, [-1])
-                flat_targ_pattern = tf.tile(flat_obs_pattern,
-                                            tf.stack([num_reps]))
-                targ_pattern = tf.reshape(flat_targ_pattern,
-                                          targ_pattern_shape)
+            # The target pattern is the observation pattern repeated n
+            # times. Some reshaping is required to accomplish the tiling.
+            targ_pattern_shape = [sub_seq_len * num_reps, num_bits]
+            flat_obs_pattern = tf.reshape(obs_pattern, [-1])
+            flat_targ_pattern = tf.tile(flat_obs_pattern,
+                                        tf.stack([num_reps]))
+            targ_pattern = tf.reshape(flat_targ_pattern,
+                                      targ_pattern_shape)
 
-                # Expand the obs_pattern to have two extra channels for flags.
-                # Concatenate start flag and num_reps flag to the sequence.
-                obs_flag_channel_pad = tf.zeros([sub_seq_len, 2])
-                obs_start_flag = tf.one_hot(
-                    [start_end_flag_idx], full_obs_size, on_value=1.,
-                    off_value=0.)
-                num_reps_flag = tf.one_hot(
-                    [num_repeats_channel_idx],
-                    full_obs_size,
-                    on_value=self._normalise(tf.cast(num_reps, tf.float32)),
-                    off_value=0.)
+            # Expand the obs_pattern to have two extra channels for flags.
+            # Concatenate start flag and num_reps flag to the sequence.
+            obs_flag_channel_pad = tf.zeros([sub_seq_len, 2])
+            obs_start_flag = tf.one_hot(
+                [start_end_flag_idx], full_obs_size, on_value=1.,
+                off_value=0.)
+            num_reps_flag = tf.one_hot(
+                [num_repeats_channel_idx],
+                full_obs_size,
+                on_value=self._normalise(tf.cast(num_reps, tf.float32)),
+                off_value=0.)
 
-                # note the concatenation dimensions.
-                obs = tf.concat([obs_pattern, obs_flag_channel_pad], 1)
-                obs = tf.concat([obs_start_flag, obs], 0)
-                obs = tf.concat([obs, num_reps_flag], 0)
+            # note the concatenation dimensions.
+            obs = tf.concat([obs_pattern, obs_flag_channel_pad], 1)
+            obs = tf.concat([obs_start_flag, obs], 0)
+            obs = tf.concat([obs, num_reps_flag], 0)
 
-                # Now do the same for the targ_pattern (it only has one extra
-                # channel).
-                targ_flag_channel_pad = tf.zeros([sub_seq_len * num_reps, 1])
-                targ_end_flag = tf.one_hot(
-                    [start_end_flag_idx], full_targ_size, on_value=1.,
-                    off_value=0.)
-                targ = tf.concat([targ_pattern, targ_flag_channel_pad], 1)
-                targ = tf.concat([targ, targ_end_flag], 0)
+            # Now do the same for the targ_pattern (it only has one extra
+            # channel).
+            targ_flag_channel_pad = tf.zeros([sub_seq_len * num_reps, 1])
+            targ_end_flag = tf.one_hot(
+                [start_end_flag_idx], full_targ_size, on_value=1.,
+                off_value=0.)
+            targ = tf.concat([targ_pattern, targ_flag_channel_pad], 1)
+            targ = tf.concat([targ, targ_end_flag], 0)
 
-                # Concatenate zeros at end of obs and begining of targ. This
-                # aligns them s.t. the target begins as soon as the obs ends.
-                obs_end_pad = tf.zeros([sub_seq_len * num_reps + 1,
-                                       full_obs_size])
-                targ_start_pad = tf.zeros([sub_seq_len + 2, full_targ_size])
+            # Concatenate zeros at end of obs and begining of targ. This
+            # aligns them s.t. the target begins as soon as the obs ends.
+            obs_end_pad = tf.zeros([sub_seq_len * num_reps + 1,
+                                   full_obs_size])
+            targ_start_pad = tf.zeros([sub_seq_len + 2, full_targ_size])
 
-                # The mask is zero during the obs and one during the targ.
-                mask_off = tf.zeros([sub_seq_len + 2])
-                mask_on = tf.ones([sub_seq_len * num_reps + 1])
+            # The mask is zero during the obs and one during the targ.
+            mask_off = tf.zeros([sub_seq_len + 2])
+            mask_on = tf.ones([sub_seq_len * num_reps + 1])
 
-                obs = tf.concat([obs, obs_end_pad], 0)
-                targ = tf.concat([targ_start_pad, targ], 0)
-                mask = tf.concat([mask_off, mask_on], 0)
+            obs = tf.concat([obs, obs_end_pad], 0)
+            targ = tf.concat([targ_start_pad, targ], 0)
+            mask = tf.concat([mask_off, mask_on], 0)
 
-                obs_tensors.append(obs)
-                targ_tensors.append(targ)
-                mask_tensors.append(mask)
+            obs_tensors.append(obs)
+            targ_tensors.append(targ)
+            mask_tensors.append(mask)
 
-            # End the loop over batch index.
-            # Compute how much zero padding is needed to make tensors sequences
-            # the same length for all batch elements.
-            residual_obs_pad = [
-                tf.zeros([residual_length_batch[i], full_obs_size])
-                for i in range(batch_size)
-            ]
-            residual_targ_pad = [
-                tf.zeros([residual_length_batch[i], full_targ_size])
-                for i in range(batch_size)
-            ]
-            residual_mask_pad = [
-                tf.zeros([residual_length_batch[i]]) for i in range(batch_size)
-            ]
+        # End the loop over batch index.
+        # Compute how much zero padding is needed to make tensors sequences
+        # the same length for all batch elements.
+        residual_obs_pad = [
+            tf.zeros([residual_length_batch[i], full_obs_size])
+            for i in range(batch_size)
+        ]
+        residual_targ_pad = [
+            tf.zeros([residual_length_batch[i], full_targ_size])
+            for i in range(batch_size)
+        ]
+        residual_mask_pad = [
+            tf.zeros([residual_length_batch[i]]) for i in range(batch_size)
+        ]
 
-            # Concatenate the pad to each batch element.
-            obs_tensors = [
-                tf.concat([o, p], 0) for o, p in zip(obs_tensors,
-                                                     residual_obs_pad)
-            ]
-            targ_tensors = [
-                tf.concat([t, p], 0) for t, p in zip(targ_tensors,
-                                                     residual_targ_pad)
-            ]
-            mask_tensors = [
-                tf.concat([m, p], 0) for m, p in zip(mask_tensors,
-                                                     residual_mask_pad)
-            ]
+        # Concatenate the pad to each batch element.
+        obs_tensors = [
+            tf.concat([o, p], 0) for o, p in zip(obs_tensors,
+                                                 residual_obs_pad)
+        ]
+        targ_tensors = [
+            tf.concat([t, p], 0) for t, p in zip(targ_tensors,
+                                                 residual_targ_pad)
+        ]
+        mask_tensors = [
+            tf.concat([m, p], 0) for m, p in zip(mask_tensors,
+                                                 residual_mask_pad)
+        ]
 
-            # Concatenate each batch element into a single tensor.
-            obs = tf.reshape(tf.concat(obs_tensors, 1), obs_batch_shape)
-            targ = tf.reshape(tf.concat(targ_tensors, 1), targ_batch_shape)
-            mask = tf.transpose(
-                tf.reshape(tf.concat(mask_tensors, 0), mask_batch_trans_shape))
-            return DatasetTensors(obs, targ, mask)
+        # Concatenate each batch element into a single tensor.
+        obs = tf.reshape(tf.concat(obs_tensors, 1), obs_batch_shape)
+        targ = tf.reshape(tf.concat(targ_tensors, 1), targ_batch_shape)
+        mask = tf.transpose(
+            tf.reshape(tf.concat(mask_tensors, 0), mask_batch_trans_shape))
+        return DatasetTensors(obs, targ, mask)
 
-        def cost(self, logits, targ, mask):
-            """Calculate sigmoid cross-entropy cost."""
-            return masked_sigmoid_cross_entropy(
-                logits,
-                targ,
-                mask,
-                time_average=self.time_average_cost,
-                log_prob_in_bits=self.log_prob_in_bits)
+    def cost(self, output, task_state):
+        """Calculate sigmoid cross-entropy cost."""
+        targ = task_state.target
+        mask = task_state.mask
+        return masked_sigmoid_cross_entropy(
+            output,
+            targ,
+            mask,
+            time_average=self.time_average_cost,
+            log_prob_in_bits=self.log_prob_in_bits)
 
-        def to_human_readable(self,
-                              data,
-                              model_output=None,
-                              whole_batch=False):
-            """Return human readable version of data."""
-            obs = data.observations
-            unnormalised_num_reps_flag = self._unnormalise(
-                obs[:, :, -1:]).round()
-            obs = np.concatenate([obs[:, :, :-1], unnormalised_num_reps_flag],
-                                 axis=2)
-            data = data._replace(observations=obs)
-            return bitstring_readable(data, self.batch_size, model_output,
-                                      whole_batch)
+    def to_string(self, output, task_state):
+        """Return human readable version of data."""
+        obs = task_state.observations
+        unnormalised_num_reps_flag = self._unnormalise(
+            obs[:, :, -1:]).round()
+        obs = np.concatenate([obs[:, :, :-1], unnormalised_num_reps_flag],
+                             axis=2)
+        data = task_state._replace(observations=obs)
+        return bitstring_readable(data, self.batch_size, output)
+
+    def process_output(self, output, task_state):
+        """Change output logits to output probabilities."""
+        return tf.round(
+            tf.expand_dims(task_state.mask, -1) * tf.sigmoid(output))
